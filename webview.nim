83db4e64617a2d87e5b3caea35139813955d2f2c
Francis Lavoie <flavoie@ferlab.bio>
2021-04-20 10:09:20 +0200

    update c interface, still wip

--- webview.nim
+++ webview.nim
@@ -4,30 +4,31 @@ import tables
 {.passC: "-I" & currentSourcePath().substr(0, high(currentSourcePath()) - 4) .}
 
 when defined(linux):
-  {.passC:"`pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0`".}
-  {.passL:"`pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0`".}
+  {.passC: "`pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0`".}
+  {.passL: "`pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0`".}
 elif defined(macosx):
   {.passL: "-framework WebKit".}
 type
-  Webview* {.importc:"webview_t",header:"webview.h".} = pointer
+  Webview* {.importc: "webview_t",header: "webview.h".} = pointer
   WebviewHint* = enum
     WEBVIEW_HINT_NONE,WEBVIEW_HINT_MIN,WEBVIEW_HINT_MAX,WEBVIEW_HINT_FIXED
-  BindFn* = proc(sequ: cstring, req:cstring)
+  BindFn* = proc(sequ: cstring, req: cstring)
+type WebviewCb = proc(seq: cstring, req: cstring, arg: pointer){.cdecl.}
 
-proc create*(debug:cint,window:pointer):Webview{.importc:"webview_create",header:"webview.h".}
-proc set_title*(w:Webview,title:cstring){.importc:"webview_set_title",header:"webview.h".}
-proc set_size*(w:Webview,width:cint,height:cint,hints:WebviewHint){.importc:"webview_set_size",header:"webview.h".}
-proc navigate*(w:Webview,url:cstring){.importc:"webview_navigate",header:"webview.h".}
-proc run*(w:Webview){.importc:"webview_run",header:"webview.h".}
-proc destroy*(w:Webview){.importc:"webview_destroy",header:"webview.h".}
-proc eval*(w:Webview, js:cstring){.importc:"webview_eval", header:"webview.h".}
-proc connect*(w:Webview, name:cstring, fn:pointer, arg:pointer){.importc:"webview_bind", header:"webview.h".}
-proc init*(w:Webview, js:cstring){.importc:"webview_init", header:"webview.h".}
+proc create*(debug:cint,window:pointer): Webview{.importc: "webview_create",header: "webview.h".}
+proc set_title*(w: Webview,title: cstring){.importc: "webview_set_title",header: "webview.h".}
+proc set_size*(w: Webview,width:cint,height:cint,hints: WebviewHint){.importc: "webview_set_size",header: "webview.h".}
+proc navigate*(w: Webview,url: cstring){.importc: "webview_navigate",header: "webview.h".}
+proc run*(w: Webview){.importc: "webview_run",header: "webview.h".}
+proc destroy*(w: Webview){.importc: "webview_destroy",header: "webview.h".}
+proc eval*(w: Webview, js: cstring){.importc: "webview_eval", header: "webview.h".}
+proc connect*(w: Webview, name: cstring, fn: WebviewCb, arg: pointer){.importc: "webview_bind", header: "webview.h".}
+proc init*(w: Webview, js: cstring){.importc: "webview_init", header: "webview.h".}
 
 
 var bindCallTable = newTable[int, BindFn]()
 
-proc bindCProc(sequ:cstring, req:cstring, arg: pointer) {.exportc.} =
+proc bindCProc(sequ: cstring, req: cstring, arg: pointer) {.cdecl.} =
   let idx = cast[int](arg)
   let fn = bindCallTable[idx]
   fn(sequ, req)
